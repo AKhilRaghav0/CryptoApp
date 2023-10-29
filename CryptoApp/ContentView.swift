ef20aa8f441daa26c384690718ffd64dc7d251a2
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-10-29 11:18:03 +0530

    Color Scheme testing

--- CryptoApp/ContentView.swift
+++ CryptoApp/ContentView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Color.theme.red
+            Color.theme.background
                 .ignoresSafeArea()
+            
+            
+            
+            VStack (spacing: 40){
+                
+                Text("Accent color")
+                    .foregroundColor(.theme.accent)
+                Text("Secondary Color")
+                    .foregroundColor(.theme.secondaryText)
+                
+                Text("Red Color")
+                    .foregroundColor(.theme.red)
+                
+                Text("Green Color")
+                    .foregroundColor(.theme.green)
+            }
+             .font(.headline)
         }
     }
 }
