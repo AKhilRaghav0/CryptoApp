225547157f67c9b1a8ac81ad47aa9e3d347dfb06
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-10-29 10:24:41 +0530

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Akhil on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
