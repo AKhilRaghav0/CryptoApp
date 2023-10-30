d30ec02b9eee73eb51f0e207baf24c2eb3814936
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-10-31 03:58:22 +0530

    Core Update
    CoinDataService Added for API calls
    HomeViewModel updated with minor animations

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject{
 
@@ -13,13 +14,27 @@ class HomeViewModel: ObservableObject{
     @Published var portfolioCoins: [CoinModel] = []
     
     
-     
+    private let dataService = CoinDataService()
+    private var cancellables = Set<AnyCancellable>()
+    
+    
     init(){
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
-            self.allCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins .append(DeveloperPreview.instance.coin)
-        }
+        addSubscribers()
         
         
     }
-}
+    
+    func addSubscribers(){
+        dataService.$allCoins
+            .sink { [weak self](returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables )
+        
+    }
+        
+    }
+    
+    
+
+
