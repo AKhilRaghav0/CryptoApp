f668a2999fb402be1ed341e34fe5509d0fae0692
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-10-30 12:10:58 +0530

    Model Update
    CoinModel introduced
    PreviewProvider Added

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,23 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Akhil on 30/10/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin: CoinModel
+    
+    var body: some View {
+        
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin:  dev.coin)
+    }
+}
---
+++ CryptoApp/Extensions/PreviewProvider.swift
@@ -0,0 +1,49 @@
+   
+
+import Foundation
+import SwiftUI
+
+
+extension PreviewProvider {
+    static var dev: DeveloperPreview{
+        return DeveloperPreview.instance 
+    }
+     
+}
+
+class DeveloperPreview {
+    
+    static let instance  = DeveloperPreview()
+    init() {}
+    
+    let coin = CoinModel(
+        id: "bitcoin",
+        symbol: "btc",
+        name: "Bitcoin",
+        image: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1696501400",
+        currentPrice: 34339,
+        marketCap: 669235927353,
+        marketCapRank: 1,
+        fullyDilutedValuation: 719707359424,
+        totalVolume: 7108542216,
+        high24H: 34345,
+        low24H: 33950,
+        priceChange24H: 197.67,
+        priceChangePercentage24H: 0.57,
+        marketCapChange24H: 83293,
+        marketCapChangePercentage24H: 8932,
+        circulatingSupply: 389322,
+        totalSupply: 83298,
+        maxSupply: 7329,
+        ath: 78372,
+        athChangePercentage: -50.32,
+        athDate: "2021-11-10T14:24:11.849Z",
+        atl: 67.81,
+        atlChangePercentage: 50421.25768,
+        atlDate: "2013-07-06T00:00:00:00Z",
+        lastUpdated: "2023-10-29T12:13:20.134Z",
+        sparklineIn7D: SparklineIn7D(price: [2994.30, 320392489, 932093
+        ]), 
+        priceChangePercentage24HInCurrency: 0.57, CurrentHoldings: 1.5)
+}
+let newInstance = DeveloperPreview()
